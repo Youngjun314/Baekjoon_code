597fcbe9daf7a066f54c20548dcc03bccac05d6b
Youngjun314 <140683583+Youngjun314@users.noreply.github.com>
2024-02-06 12:00:58 +0900

    [Bronze I] Title: 제자리, Time: 44 ms, Memory: 31120 KB, Score: 49 point -BaekjoonHub

--- 백준/Bronze/25400. 제자리/제자리.py
+++ 백준/Bronze/25400. 제자리/제자리.py
@@ -1,12 +1,10 @@
 n = int(input())
 l = list(map(int, input().split()))
-l.sort
 cnt = 0
-index = 0
 for i in range(n):
-    if i + 1 in l and index <= l.index(i + 1):
+    if i + 1 in l:
+        del l[:l.index(i + 1) + 1]
         cnt += 1
-        index = l.index(i + 1)
     else:
         break
 print(n - cnt)
