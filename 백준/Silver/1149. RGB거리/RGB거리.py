27a468972c32de174f97c6adfd38eec7ee1aebf3
Youngjun314 <140683583+Youngjun314@users.noreply.github.com>
2025-05-31 18:29:58 +0900

    [Silver I] Title: RGB거리, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,15 +1,14 @@
 n = int(input())
-cost = [list(map(int, input().split())) for _ in range(n)]
+l = [list(map(int, input().split())) for _ in range(n)]
+l2 = [[0] * 3 for _ in range(n)]
 
-dp = [[0] * 3 for _ in range(n)]
-
-dp[0][0] = cost[0][0]  
-dp[0][1] = cost[0][1]  
-dp[0][2] = cost[0][2] 
+l2[0][0] = l[0][0]  
+l2[0][1] = l[0][1]  
+l2[0][2] = l[0][2] 
 
 for i in range(1, n):
-    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + cost[i][0]
-    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + cost[i][1]
-    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + cost[i][2]
+    l2[i][0] = min(l2[i-1][1], l2[i-1][2]) + l[i][0]
+    l2[i][1] = min(l2[i-1][0], l2[i-1][2]) + l[i][1]
+    l2[i][2] = min(l2[i-1][0], l2[i-1][1]) + l[i][2]
 
-print(min(dp[n-1]))
+print(min(l2[n-1]))
